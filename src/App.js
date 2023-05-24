e6a562ac3c0ac953d221fc5bb7d9e209463d9bc5
Your Name <you@example.com>
2023-05-23 19:36:57 -0500

    remove tools

--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ function Light() {
 export default function App() {
   const [bad, set] = useState(false)
   const { impl, debug, enabled, samples, ...config } = useControls({
-    debug: true,
-    enabled: true,
+    debug: false,
+    enabled: false,
     size: { value: 35, min: 0, max: 100, step: 0.1 },
     focus: { value: 0.5, min: 0, max: 2, step: 0.1 },
     samples: { value: 16, min: 1, max: 40, step: 1 }
