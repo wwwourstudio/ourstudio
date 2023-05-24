0f0b49cff638e9f531fb70d9830bffa7ea74d333
Your Name <you@example.com>
2023-05-23 20:45:25 -0500

    a

--- src/App.js
+++ src/App.js
@@ -20,13 +20,6 @@ function Light() {
 
 export default function App() {
   const [bad, set] = useState(false)
-  const { impl, debug, enabled, samples, ...config } = useControls({
-    debug: true,
-    enabled: true,
-    size: { value: 35, min: 0, max: 100, step: 0.1 },
-    focus: { value: 0.5, min: 0, max: 2, step: 0.1 },
-    samples: { value: 16, min: 1, max: 40, step: 1 }
-  })
   return (
     <Canvas shadows camera={{ position: [5, 2, 10], fov: 50 }}>
       {enabled && <SoftShadows {...config} samples={bad ? Math.min(6, samples) : samples} />}
