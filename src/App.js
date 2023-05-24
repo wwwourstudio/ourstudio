47136ce1bb341286bf87a66e84b83ef4d5f273ed
Your Name <you@example.com>
2023-05-24 00:54:39 -0500

    aaa

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import { Canvas, useFrame } from "@react-three/fiber"
 import { SoftShadows, Float, CameraControls, Sky} from "@react-three/drei"
 import { easing } from "maath"
 import { Model as Room } from "./Room"
+import { Logo } from "@pmndrs/branding"
+
+function Overlay() {
+  return (
+    <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
+      <a href="https://pmnd.rs/" style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '13px' }}>
+        pmnd.rs
+        <br />
+        dev collective
+      </a>
+      <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}>ok —</div>
+      <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>16/12/2022</div>
+    </div>
+  )
+}
 
 function Light() {
   const ref = useRef()
@@ -33,6 +48,8 @@ export default function App() {
       <Sphere position={[2, 4, -8]} scale={0.9} />
       <Sphere position={[-2, 2, -8]} scale={0.8} />
       <Sky inclination={0.52} scale={20} />
+      <Overlay />
+    <Logo style={{ position: 'absolute', bottom: 40, left: 40, width: 30 }} />
     </Canvas>
   )
 }
