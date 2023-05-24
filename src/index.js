f972bb870a9098175fca37429872b4a20ea8913b
Your Name <you@example.com>
2023-05-23 19:48:06 -0500

    edit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client"
 import "./styles.css"
 import App from "./App"
-import { Leva } from "leva"
 
 createRoot(document.getElementById("root")).render(
   <>
